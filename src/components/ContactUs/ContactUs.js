9dcb14f588fb2f35473049fdaf5a2c504f188618
tedelcopp <tomas.edelcopp@gmail.com>
2023-03-18 13:17:48 -0300

    first commit

---
+++ src/components/ContactUs/ContactUs.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import style from "./ContactUs.css";
+
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+        <div className="row">
+          <div className="ten columns">
+            <p className="lead">
+              Feel free to contact me for any work or suggestions below
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <aside className="eigth columns footer-widgets" />
+        </div>
+      </section>
+    );
+  }
+}
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,107 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Tomás Edelcopp",
+  role: "Full Stack Developer with Front End orientation",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/edelcopp/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/tedelcopp",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "Buenos Aires, Argentina.",
+  website: "tomas.edelcopp@gmail.com",
+  education: [
+    {
+      UniversityName: "Soy Henry",
+      specialization: "Full Stack Web Developer",
+      MonthOfPassing: "April",
+      YearOfPassing: "2023",
+      Achievements:
+        "Full Stack developer intensive Bootcamp of over 800 hours of coding. Technologies learned: Javascript, React, Redux, HTML5, CSS, Node, Express, PostgreSQL and Sequelize",
+    },
+    {
+      UniversityName: "UTN",
+      specialization: "Digital Marketing and Social Media",
+      MonthOfPassing: "Nov",
+      YearOfPassing: "2020",
+      // Achievements: "a"
+    },
+  ],
+  work: [
+    {
+      CompanyName: "The Green Darwin",
+      specialization: "CEO",
+      MonthOfLeaving: "Mar",
+      YearOfLeaving: "2023",
+      Achievements:
+        "My passion for gardening and my entrepreneurial spirit motivated me to create my own business to finance my university studies. I learned how to program and developed an e-commerce platform for plants and gardening supplies. In my online store, I offer a wide variety of products to meet the needs of any gardening enthusiast. From beautiful plants to high-quality gardening tools, my business has everything you need to create and maintain a beautiful garden at home.",
+    },
+    {
+      CompanyName: "Faena Buenos Aires",
+      specialization: "Supervisor",
+      MonthOfLeaving: "Aug",
+      YearOfLeaving: "2021",
+      Achievements:
+        "I successfully led and directed rotating teams in a wide variety of social and corporate events, ensuring the achievement of specific objectives and respecting the required quality standard. Additionally, I demonstrated effective skills in communication, conflict resolution, employee training and development, which contributed to the success in the planning and execution of the events. I worked collaboratively with other supervisors and managers to achieve short, medium and long-term company goals, ensuring a productive and motivating work environment. During my performance, I remained committed to the pursuit of excellence in work and high quality standards within the area.",
+    },
+  ],
+  // skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "JavaScript",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "NodeJS",
+    },
+    {
+      skillname: "Sequelize",
+    },
+  ],
+  portfolio: [
+    {
+      name: "The Green Darwin - Project",
+      description: "App",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "Dogs Finder - Project",
+      description: "App",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "Evant - Project",
+      description: "App",
+      imgurl: "images/portfolio/project2.png",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Nietzsche",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
