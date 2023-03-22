51c8087741adc2ea7313c7906c8fb1e940f51d6b
tedelcopp <tomas.edelcopp@gmail.com>
2023-03-22 16:38:40 -0300

    wip

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -9,7 +9,7 @@ export default class ContactUs extends Component {
         <div className="row">
           <div className="ten columns">
             <p className="lead">
-              Feel free to contact me for any work or suggestions below
+              Feel free to contact me for any work or suggestions below!
             </p>
           </div>
         </div>
