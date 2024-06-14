aebe48acf682312bfa24fa49e94dd1910c8b565b
tedelcopp <tomas.edelcopp@gmail.com>
2024-06-14 00:11:28 -0300

    About&Social.WIP

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class About extends Component {
   handleLinkClick = (event) => {
-    const links = document.querySelectorAll('.contact-details a');
-    links.forEach((link) => link.removeAttribute('data-active'));
-    event.target.setAttribute('data-active', 'true');
+    const links = document.querySelectorAll(".contact-details a");
+    links.forEach((link) => link.removeAttribute("data-active"));
+    event.target.setAttribute("data-active", "true");
   };
 
   render() {
     let resumeData = this.props.resumeData;
- 
+
     return (
       <section id="about">
-      <div className="row">
-        <div className="eleven columns main-col centered">
-          <div className="about-me">
-            <h2>ABOUT ME</h2>
-            <div className="profile-pic-container">
-              <img className="profile-pic" src="images/profileprofile.png" alt="" />
+        <div className="row">
+          <div className="eleven columns main-col centered">
+            <div className="about-me">
+              <h2>ABOUT ME</h2>
+              <div className="profile-pic-container">
+                <img
+                  className="profile-pic"
+                  src="images/profileprofile.png"
+                  alt=""
+                />
+              </div>
+              <p>
+                Hello!<span className="wave">👋</span>
+                {resumeData.aboutme1}{" "}
+              </p>
+              <p>{resumeData.aboutme2}</p>
             </div>
-            <p>Hello!<span className="wave">👋</span>{resumeData.aboutme1} </p>
-            <p>{resumeData.aboutme2}</p>
           </div>
-                </div>
-   </div>
+        </div>
       </section>
     );
   }
--- src/components/Social.js
+++ src/components/Social.js
@@ -7,47 +7,45 @@ export default class Social extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
-      <div className="contact-details">
-        <h2>Contact Details</h2>
-        <div>
-          <button className="social-button linkedin-link">
-            <a
-              href="https://www.linkedin.com/in/edelcopp/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedin className="social-link" />
-            </a>
-          </button>
-        </div>
-        <div>
-          <button className="github-link">
-            <a
-              href="https://github.com/tedelcopp"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FiGithub className="social-link" />
-            </a>
-          </button>
-        </div>
-        <div>
-          <button className="gmail-link">
-            <a href="mailto:tomas.edelcopp@gmail.com">
-              <SiGmail className="social-link" />
-            </a>
-          </button>
-        </div>
-        <div>
-          <button className="jumping resume-button">
-            <a
-              href="https://drive.google.com/file/d/1u-iyRovLAIHLdCFwxu7pTqVXfgY4fkjn/view?usp=drive_link"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Check out my resume!
-            </a>
-          </button>
+      <div className="contact-details-container">
+        <div className="contact-details">
+          <h2>Contact Details</h2>
+          <div className="social-buttons">
+            <button className="social-button linkedin-link">
+              <a
+                href="https://www.linkedin.com/in/edelcopp/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin className="social-link" />
+              </a>
+            </button>
+            <button className="social-button github-link">
+              <a
+                href="https://github.com/tedelcopp"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FiGithub className="social-link" />
+              </a>
+            </button>
+            <button className="social-button gmail-link">
+              <a href="mailto:tomas.edelcopp@gmail.com">
+                <SiGmail className="social-link" />
+              </a>
+            </button>
+          </div>
+          <div className="resume-button">
+            <button className="jumping">
+              <a
+                href="https://drive.google.com/file/d/1u-iyRovLAIHLdCFwxu7pTqVXfgY4fkjn/view?usp=drive_link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Check out my resume!
+              </a>
+            </button>
+          </div>
         </div>
       </div>
     );
