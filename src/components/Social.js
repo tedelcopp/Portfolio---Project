5d6041e76b194a8975b7229e246b1078df4bacef
tedelcopp <tomas.edelcopp@gmail.com>
2024-12-19 23:47:17 -0300

    WIP

--- src/components/Social.js
+++ src/components/Social.js
@@ -37,7 +37,7 @@ export default class Social extends Component {
         </div>
         <div className="social-contact-resume">
           <a
-            href="https://drive.google.com/file/d/1ecpL6NLlJ6Q450kqJZ26b4PiqfJ0PxwJ/view?usp=sharing"
+            href="https://drive.google.com/file/d/1mGsUtrjCUQedGes09X14H7AixURzNSxO/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="social-contact-resume-link cv-button"
