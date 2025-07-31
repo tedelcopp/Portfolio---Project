f7193fba46b518029b3e7d478cde422399045968
tedelcopp <tomas.edelcopp@gmail.com>
2025-07-31 11:36:03 -0300

    Portfolio.WIP

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,6 +6,51 @@ export default class Portfolio extends Component {
   render() {
     let resumeData = this.props.resumeData;
 
+    const highlightKeywords = (text, keywords) => {
+      let result = text;
+      keywords.forEach((keyword) => {
+        const regex = new RegExp(`\\b(${keyword})\\b`, "gi");
+        result = result.replace(regex, "<strong>$1</strong>");
+      });
+      return result;
+    };
+
+    const keywords = [
+      "NextJS",
+      "Next.js",
+      "React",
+      "Tailwind CSS",
+      "Node.js",
+      "Express",
+      "Sequelize",
+      "MySQL",
+      "DBeaver",
+      "TypeScript",
+      "React Hot Toast",
+      "React Lucide",
+      "WordPress",
+      "Landing Page",
+      "frontend",
+      "backend",
+      "navegación",
+      "optimización",
+      "notificaciones",
+      "iconografía",
+      "modular",
+      "escalable",
+      "freelance",
+      "compatibilidad",
+      "experiencia del usuario",
+      "estructura",
+      "visualización",
+      "diseño moderno",
+      "acciones estratégicas",
+      "conversión",
+      "Hot",
+      "Toast",
+      "Lucide",
+    ];
+
     return (
       <section id="portfolio">
         <h1>Proyectos</h1>
@@ -28,12 +73,23 @@ export default class Portfolio extends Component {
                     <section id="skills">
                       <div className="portfolio-card-content">
                         <h2>{item.name}</h2>
-                        <p>{item.description}</p>
-
+                        <p
+                          dangerouslySetInnerHTML={{
+                            __html: highlightKeywords(
+                              item.description,
+                              keywords
+                            ),
+                          }}
+                        />
                         <div className="portfolio-footer">
-                          <p>
-                            • <u>Tecnologías:</u> {item.stack}
-                          </p>
+                          <p
+                            dangerouslySetInnerHTML={{
+                              __html:
+                                "• <u>Tecnologías:</u> " +
+                                highlightKeywords(item.stack, keywords),
+                            }}
+                          />
+
                           <div className="portfolio-button-container">
                             <a
                               href={item.urlgithub}
